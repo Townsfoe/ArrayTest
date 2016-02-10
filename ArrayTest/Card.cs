cb8796dcc86572eafc03a78fabf99a3d996007ed
Jacob Townsend <jrtownsend776@gmail.com>
2016-02-10 18:30:34 -0500

    First commit
    Testing card class. So far so good.

---
+++ ArrayTest/Card.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTest
+{
+    class Card
+
+    {
+        private String Name;
+        private int cost;
+        
+        public Card()
+        { }
+        
+        public Card(String str, int num)
+        {
+            this.Name = str;
+            this.cost = num;
+
+        }
+
+        public String getName()
+        {
+            return this.Name;
+        }
+
+        public int Cost
+        {
+            get
+            {
+                return this.cost;
+            }
+        }
+    }
+}
---
+++ ArrayTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Card testCard = new Card("Testcard", 10);
+            Console.WriteLine(testCard.getName());
+            Console.WriteLine(testCard.Cost);
+            Console.Read();
+        }
+    }
+}
